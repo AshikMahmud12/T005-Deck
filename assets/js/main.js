aa3e443f6d86a2ae5d3462313152616ba77d932e
ashikmahmud12345@gmail.com <ashikmahmud12345@gmail.com>
2020-04-23 22:56:45 +0600

    Fourth Time Upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,10 +12,24 @@ $(document).ready(function () {
     var mixer = mixitup(".container");
 });
 
-
 function openNav() {
     document.getElementById("myNav").style.width = "100%";
 }
 function closeNav() {
     document.getElementById("myNav").style.width = "0%";
 }
+
+(function ($) {
+    "use strict";
+
+    /*--------------------------------------
+        Magnific popup Active
+    ----------------------------------------*/
+    $(".img-thumbnail").magnificPopup({
+        delegate: "a",
+        type: "image",
+        gallery: {
+            enabled: true,
+        },
+    });
+})(jQuery);
